f762f1114c48ee06e644225f6dc632c00d746be8
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-08-22 01:14:45 +0530

    Inserting into trie done ✅

--- TriesImpl.java
+++ TriesImpl.java
@@ -15,10 +15,28 @@ class TrieNode{
     public boolean getIsEnd(){
         return this.isEnd;
     }
+    public void setIsEnd(boolean bool){
+        this.isEnd = bool;
+    }
 }
 
 public class TriesImpl {
     public static void main(String[] args) {
+        TrieNode curr = new TrieNode();
+        String str = "Abhishek";
+        insertIntoTrie(str, curr);
+    }
+    public static void insertIntoTrie(String str, TrieNode curr){
+        for (int j=0; j<str.length(); j++){
+            char ch = str.charAt(j);
+            if (!curr.getMap().containsKey(ch)){
+                curr.getMap().put(ch, new TrieNode());
+            }
+            curr = curr.getMap().get(ch);
+        }
+        curr.setIsEnd(true);
+    }
+    public static void searchIntoTrie(String str, TrieNode curr){
         
     }
 }
